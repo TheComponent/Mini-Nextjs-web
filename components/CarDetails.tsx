63652d4651924bd7fba4836b143a67b4d02f9c64
Khanh Du <luongkhanhdu95@gmail.com>
2024-12-05 16:43:59 +0700

    feat: add show more button

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -27,7 +27,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
           >
-            <div className='fixed inset-0 bg-black border-opacity-25' />
+            <div className='fixed inset-0 bg-black border-opacity-25 opacity-50' />
           </TransitionChild>
 
           <div className='fixed inset-0 overflow-y-auto'>
@@ -37,12 +37,11 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 enter='ease-out duration-300'
                 enterFrom='opacity-0 scale-95'
                 enterTo='opacity-100 scale-100'
-                leave='ease-in duration-200'
+                leave='ease-out duration-300'
                 leaveFrom='opacity-100 scale-100'
                 leaveTo='opacity-0 scale-95'
               >
-                <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white
-                  p-6 text-left shadow-xl transition-all flex flex-col gap-5">
+                <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                     type='button'
                     className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
