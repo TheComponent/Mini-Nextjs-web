3c30320aa813d06cee9960fda4c0f867f7510119
Khanh Du <110228244+khanhduzz@users.noreply.github.com>
2024-12-05 14:34:16 +0700

    feat: add search with param (#16)

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,10 +4,12 @@ import React from 'react'
 import Image from 'next/image'
 import { useState } from 'react'
 import SearchManufacturer from './SearchManufacturer'
+import { useRouter } from 'next/navigation'
+
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
     <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
-        <Image 
+        <Image
             src="/magnifying-glass.svg"
             alt='magnifying glass'
             width={40}
@@ -21,8 +23,37 @@ const SearchBar = () => {
 
     const [manufacturer, setManufacturer] = useState('');
     const [model, setModel] = useState('');
+    const router = useRouter();
 
-    const handleSearch = () => { }
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (manufacturer === '' && model === '') {
+            return alert('Fill in the search bar')
+        }
+
+        updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+    }
+
+    const updateSearchParams = (model : string, manufacturer : string) => {
+        const searchParams = new URLSearchParams(window.location.search);
+
+        if (model) {
+            searchParams.set('model', model);
+        } else {
+            searchParams.delete('model');
+        }
+        
+        if (manufacturer) {
+            searchParams.set('manufacturer', manufacturer);
+        } else {
+            searchParams.delete('manufacturer');
+        }
+
+        const newPath = `${window.location.pathname}?${searchParams.toString()}`
+
+        router.push(newPath)
+    }
 
     return (
         <form action="" className='searchbar gap-2' onSubmit={handleSearch}>
@@ -35,15 +66,15 @@ const SearchBar = () => {
             </div>
 
             <div className='searchbar__item'>
-                <Image 
+                <Image
                     src="/model-icon.png"
                     width={25}
                     height={25}
                     className='absolute w-[20px] h-[20px] ml-4'
                     alt='car model'
                 />
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     name='model'
                     value={model}
                     onChange={(e) => setModel(e.target.value)}
--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "x-rapidapi-key": "677d638cf2msh9ec367fdb4a60a1p152f16jsn960af3b88042",
     "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
@@ -38,12 +40,12 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   const { make, year, model } = car;
 
-  url.searchParams.append('customer', 'hrjavascript-mastery');
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(' ')[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
-  url.searchParams.append('angle', `${angle}`);
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
 
   return `${url}`;
 };
